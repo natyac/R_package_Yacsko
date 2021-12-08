c8ed576c649f868af87c31acc3de5302c788b524
natyac <natasha.yacsko@gmail.com>
2021-12-08 22:02:29 +0000

    R package final.

---
+++ AntHistory.R
@@ -0,0 +1,17 @@
+#' 3rd R command.
+#' Input: The minimum age (min_age) of the ants
+#' Output: A histogram showing the minimum age in which the ants lived.
+#' @param Ants is the data set Ants.csv
+#' @param ColumnGenus is the genus of the Ants, and variable column being graphed
+#' @return GenusCount A histogram of the minimum age of the ants.
+#' @export
+
+AntsIn <- function (Ants,ColumnGenus){
+  if (!is.data.frame(Ants)){
+    return("Error in data formation")
+}
+  GenusCount <- Ants %>% 
+  select({{ColumnGenus}})
+  count({{ColumnGenus}})
+  return(GenusCount)
+}
--- R/cleanup.R
+++ R/cleanup.R
@@ -1,13 +1,15 @@
 #' First R script.
-#' Input: removes NA values
-#' Output: a data set free of missing values
+#' Input: Data set with NA values and missing data to be read.
+#' Output: Data set free of NA values with converted missing values to NA.
 
 #' Remove NA values from a data set.
-#' @param input_value data with NA values
+#' @param SixLegs data with NA values but needs to be typed as
+#'  "/cloud/projects/data/FossilAnts.csv"
 #' @return output_value
 #' @usage cleanup(data)
+#' @export
 
-cleanup <- function (inputs){
+ByeByeNA <- function (SixLegs){
   nonNADataFrame <- read_csv(SixLegs, na = c("None", "NA"))
   nonNADataFrame <-na.omit(nonNADataFrame)
   if (any(is.na(nonNADataFrame)) == TRUE){
@@ -15,4 +17,3 @@ cleanup <- function (inputs){
   }
   return(nonNADataFrame)
 }
-
